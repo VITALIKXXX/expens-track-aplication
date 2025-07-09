07f950b7896b94ceaa9efa7340b95e57ae5f8eb3
Vitalii Tomenko <vitalikxxx985@gmail.com>
2025-07-09 21:54:35 +0200

    deleted

--- src/components/SubHeader/index.js
+++
@@ -1,9 +0,0 @@
-import { StyledSubHeader, Title } from "./styled";
-
-const SubHeader = ({ title }) => (
-    <StyledSubHeader>
-        <Title>{title}</Title>
-    </StyledSubHeader>
-);
-
-export default SubHeader;
